9ff74a9427ba78e4e15b3fed146377fae4215e53
Maida <maida.maxime@gmail.com>
2023-01-04 21:53:07 +0100

    Add quality for item

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
 const weaponType = ["Sword", "Axe", "Slingshot", "Staff"];
 const armorType = ["Armor", "Gloves", "Boots"];
+//const quality = ["Common", "Magic", "Rare", "Epic", "Legendary"];
 
 const randomizeArray = (array) => {
   return Math.floor(Math.random() * array.length);
 };
 
-const displayItem = () => {};
+const displayItem = (item) => {
+  // Affichage d'une arme
+  if (item.damage) {
+    document.getElementById(
+      "p"
+    ).innerHTML = `${item.finalDrop} ${item.quality}<br>
+                      Damage: ${item.damage}`;
+    document.getElementById("img").setAttribute("src", item.imgSrc);
+  } else {
+    // Affichage d'une armure
+    if (item.armor) {
+      document.getElementById(
+        "p"
+      ).innerHTML = ` ${item.finalDrop} ${item.quality}<br>
+                      armor ${item.armor}<br>
+                      Fire Resistance ${item.fireRes}<br>
+                      Cold Resistance ${item.coldRes}<br>
+                      Lightning Resistance ${item.lightningRes}<br>
+                      Poison Resistance ${item.poisonRes}
+                      
+      `;
+      document.getElementById("img").setAttribute("src", item.imgSrc);
+    }
+  }
+};
+
+const stats = () => {
+  return Math.floor(Math.random() * (11 - 1) + 1);
+};
+
+const qualityMultiplier = () => {};
 // Version en dehors de la class, quel est le mieux ???
 // const matchImg = (weapon) => {
 //   if (weapon.finalDrop === "Sword") {
@@ -17,9 +48,11 @@ const displayItem = () => {};
 // };
 
 class Weapon {
-  damage = Math.floor(Math.random() * (11 - 1) + 1);
+  damage = stats();
   finalDrop = weaponType[randomizeArray(weaponType)];
   imgSrc = this.matchImg();
+  numberQuality = Math.floor(Math.random() * (1400 - 1) + 1);
+  quality = this.checkQuality();
 
   matchImg() {
     if (this.finalDrop === "Sword") {
@@ -33,16 +66,32 @@ class Weapon {
     } else {
     }
   }
+
+  checkQuality() {
+    if (this.numberQuality > 1 && this.numberQuality < 600) {
+      return "Common";
+    } else if (this.numberQuality >= 600 && this.numberQuality < 1000) {
+      return "Magic";
+    } else if (this.numberQuality >= 1000 && this.numberQuality < 1200) {
+      return "Rare";
+    } else if (this.numberQuality >= 1200 && this.numberQuality <= 1399) {
+      return "Epic";
+    } else if (this.numberQuality == 1400) {
+      return "Légendaire";
+    }
+  }
 }
 
 class Armor {
-  armor = 2;
-  fireRes = 5;
-  coldRes = 5;
-  lightningRes = 5;
-  poisonRes = 5;
+  armor = stats();
+  fireRes = stats();
+  coldRes = stats();
+  lightningRes = stats();
+  poisonRes = stats();
   finalDrop = armorType[randomizeArray(armorType)];
   imgSrc = this.matchImg();
+  numberQuality = Math.floor(Math.random() * (1400 - 1) + 1);
+  quality = this.checkQuality();
 
   matchImg() {
     if (this.finalDrop === "Armor") {
@@ -54,6 +103,20 @@ class Armor {
     } else {
     }
   }
+
+  checkQuality() {
+    if (this.numberQuality > 1 && this.numberQuality < 600) {
+      return "Common";
+    } else if (this.numberQuality >= 600 && this.numberQuality < 1000) {
+      return "Magic";
+    } else if (this.numberQuality >= 1000 && this.numberQuality < 1200) {
+      return "Rare";
+    } else if (this.numberQuality >= 1200 && this.numberQuality <= 1399) {
+      return "Epic";
+    } else if (this.numberQuality == 1400) {
+      return "Légendaire";
+    }
+  }
 }
 
 document.getElementById("btn").addEventListener("click", function (e) {
@@ -61,9 +124,12 @@ document.getElementById("btn").addEventListener("click", function (e) {
   const armor = new Armor();
   const allClass = [armor, weapon];
   const itemGenerated = allClass[randomizeArray(allClass)];
-  document.getElementById(
-    "p"
-  ).innerHTML = `${itemGenerated.finalDrop} Damage: ${itemGenerated.damage} `;
-  document.getElementById("img").setAttribute("src", itemGenerated.imgSrc);
   console.log(itemGenerated);
+  displayItem(itemGenerated);
 });
+
+// item quality 1400: common -> 1-599
+//                    Magic  -> 600-999
+//                    Rare   -> 1000-1199
+//                    Epic   -> 1200-1299
+//                    Legendaire -> 1400
