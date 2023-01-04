7015d107241c6ba37ec5857ff89c717b4d061d96
Maida <maida.maxime@gmail.com>
2023-01-04 20:02:28 +0100

    Add randomize between multiple class

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const weaponType = ["Sword", "Sabre", "Dague", "Fléau", "Masse", "Axe"];
+const weaponType = ["Sword", "Axe", "Slingshot", "Staff"];
+const armorType = ["Armor", "Gloves", "Boots"];
 
-const randomWeapon = () => {
-  return Math.floor(Math.random() * weaponType.length);
+const randomizeArray = (array) => {
+  return Math.floor(Math.random() * array.length);
 };
+
+const displayItem = () => {};
 // Version en dehors de la class, quel est le mieux ???
 // const matchImg = (weapon) => {
 //   if (weapon.finalDrop === "Sword") {
@@ -15,7 +18,7 @@ const randomWeapon = () => {
 
 class Weapon {
   damage = Math.floor(Math.random() * (11 - 1) + 1);
-  finalDrop = weaponType[randomWeapon()];
+  finalDrop = weaponType[randomizeArray(weaponType)];
   imgSrc = this.matchImg();
 
   matchImg() {
@@ -23,6 +26,31 @@ class Weapon {
       return "./public/assets/Sword.png";
     } else if (this.finalDrop === "Axe") {
       return "./public/assets/Axe.png";
+    } else if (this.finalDrop === "Slingshot") {
+      return "./public/assets/Slingshot.png";
+    } else if (this.finalDrop === "Staff") {
+      return "./public/assets/Staff.png";
+    } else {
+    }
+  }
+}
+
+class Armor {
+  armor = 2;
+  fireRes = 5;
+  coldRes = 5;
+  lightningRes = 5;
+  poisonRes = 5;
+  finalDrop = armorType[randomizeArray(armorType)];
+  imgSrc = this.matchImg();
+
+  matchImg() {
+    if (this.finalDrop === "Armor") {
+      return "./public/assets/Armor.png";
+    } else if (this.finalDrop === "Gloves") {
+      return "./public/assets/Gloves.png";
+    } else if (this.finalDrop === "Boots") {
+      return "./public/assets/Boots.png";
     } else {
     }
   }
@@ -30,8 +58,12 @@ class Weapon {
 
 document.getElementById("btn").addEventListener("click", function (e) {
   const weapon = new Weapon();
+  const armor = new Armor();
+  const allClass = [armor, weapon];
+  const itemGenerated = allClass[randomizeArray(allClass)];
   document.getElementById(
     "p"
-  ).innerHTML = `${weapon.finalDrop} Damage: ${weapon.damage} `;
-  document.getElementById("img").setAttribute("src", weapon.imgSrc);
+  ).innerHTML = `${itemGenerated.finalDrop} Damage: ${itemGenerated.damage} `;
+  document.getElementById("img").setAttribute("src", itemGenerated.imgSrc);
+  console.log(itemGenerated);
 });
